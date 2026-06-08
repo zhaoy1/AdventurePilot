3eb42c0e2e53b5cab28d831ab6b1a01e5262864a
Zhao <zhaoyubo@gmail.com>
2026-06-07 21:10:56 -0700

    Fix cruise speed stalk control for Rivian with openpilot longitudinal
    - Set pcmCruise=False when longitudinal control is active so openpilot
      manages set speed via stalk inputs instead of reading stock cruise
    - Return False for round_to_nearest in default increment mode to avoid
      snap-to-grid behavior that causes inconsistent speed changes

--- opendbc_repo/opendbc/car/rivian/interface.py
+++ opendbc_repo/opendbc/car/rivian/interface.py
@@ -29,6 +29,7 @@ class CarInterface(CarInterfaceBase):
     ret.alphaLongitudinalAvailable = True
     if alpha_long:
       ret.openpilotLongitudinalControl = True
+      ret.pcmCruise = False
       ret.safetyConfigs[0].safetyParam |= RivianSafetyFlags.LONG_CONTROL.value
 
     ret.longitudinalActuatorDelay = 0.15
@@ -51,6 +52,7 @@ class CarInterface(CarInterfaceBase):
 
     if alpha_long and stock_cp.alphaLongitudinalAvailable:
       stock_cp.openpilotLongitudinalControl = True
+      stock_cp.pcmCruise = False
       stock_cp.safetyConfigs[0].safetyParam |= RivianSafetyFlags.LONG_CONTROL.value
       if not (ret.flags & RivianFlagsSP.LONGITUDINAL_HARNESS_UPGRADE):
         ret.flags |= RivianFlagsSP.LONGITUDINAL_WITHOUT_HARNESS.value
