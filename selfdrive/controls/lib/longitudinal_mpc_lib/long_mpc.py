e68f2a817bf7b78c093ccbd7302a44d37d61cc72
Zhao <zhaoyubo@gmail.com>
2026-06-15 21:06:25 -0700

    Set standard T_FOLLOW to 1.25 (previous aggressive value)

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,7 +74,7 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
+    return 1.25
   elif personality==log.LongitudinalPersonality.aggressive:
     return 1.0
   else:
